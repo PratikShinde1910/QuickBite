dec9e1a4e1bdefb67a3c14757f2e6e52d42536ee
Pratik rajendra shinde <pratikrajendrashinde@Mac.lan>
2026-02-23 16:45:59 +0530

    push to render

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,9 @@ app.get("/", (req, res) => {
     res.send("QuickBite API Running");
 });
 
-const PORT = 5055;
+const PORT = process.env.PORT || 5055;
 
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
 
